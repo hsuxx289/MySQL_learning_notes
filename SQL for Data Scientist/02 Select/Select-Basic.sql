531c4be2a23e7d626bed0a30110930db6a0190ff
unknown <boweihsu07@gmail.com>
2025-03-25 18:35:28 +0800

    practice

--- SQL for Data Scientist/02 Select/Select-Basic.sql
+++ SQL for Data Scientist/02 Select/Select-Basic.sql
@@ -1,3 +1,4 @@
+# 2-3
 # 不use database 就選取到表格的方法
 SELECT *
 FROM farmers_market.product;
@@ -12,6 +13,7 @@ SELECT market_date, vendor_id, booth_number
 FROM farmers_market.vendor_booth_assignments
 LIMIT 5;
 
+# 2-4
 # 依字母排序 (預設為升冪且 NULL 會在前面)
 SELECT product_id, product_name
 FROM farmers_market.product
@@ -30,12 +32,68 @@ FROM farmers_market.vendor_booth_assignments
 ORDER BY market_date, vendor_id
 LIMIT 12;
 
+# 2-5
 # 單列欄位資料運算與別名
 SELECT 
-		market_date,
-		customer_id,
-        vendor_id,
-        quantity * cost_to_customer_per_qty AS price
+	market_date,
+	customer_id,
+	vendor_id,
+	quantity * cost_to_customer_per_qty AS price
 FROM farmers_market.customer_purchases
 ORDER BY market_date
 LIMIT 10;
+
+# 2-6
+# 四捨五入的函數
+SELECT 
+	market_date,
+	customer_id,
+	vendor_id,
+	ROUND(quantity * cost_to_customer_per_qty, 2) AS price
+FROM farmers_market.customer_purchases
+ORDER BY market_date
+LIMIT 10;
+
+# 2-7 
+# 連接字串的函數
+SELECT
+	customer_id,
+	CONCAT(customer_first_name, " ", customer_last_name)
+		AS customer_name
+FROM farmers_market.customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 5;
+
+# 函數裡的函數
+SELECT
+	customer_id,
+    UPPER(CONCAT(customer_last_name, ", ", customer_first_name))
+		AS customer_name
+FROM farmers_market.customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 5;
+
+# 函數嵌套計算
+SELECT ROUND(AVG(SUM(price)), 2) AS avg_price
+FROM sales
+GROUP BY product_id;
+
+# 函數嵌套作為參數
+SELECT CONCAT(UPPER(LEFT(first_name, 1)),
+	LOWER(SUBSTRING(first_name, 2))) AS formatted_name
+FROM customers;
+
+# 2-9
+# 回傳customer 表格中的所有欄位資料
+SELECT * FROM farmers_market.customer;
+
+# 只回傳customer 的前10筆資料 以last name 然後 first name 排序
+SELECT *
+FROM farmers_market.customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
+
+# 列出id first name 欄位 並以first name 排序
+SELECT customer_id, customer_first_name
+FROM farmers_market.customer 
+ORDER BY customer_first_name;
