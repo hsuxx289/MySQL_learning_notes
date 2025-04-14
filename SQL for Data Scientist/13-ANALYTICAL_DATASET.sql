2c44bfa601775d40ec0f19bc063c6e9218dc7b9a
unknown <boweihsu07@gmail.com>
2025-04-14 17:34:07 +0800

    practice

--- SQL for Data Scientist/13-ANALYTICAL_DATASET.sql
+++ SQL for Data Scientist/13-ANALYTICAL_DATASET.sql
@@ -354,3 +354,334 @@ ORDER BY mvi.market_year, mvi.market_week;
 
 # 13.4
 # 顧客居住地區與人口統計分析資料集
+# 以顧客為基準 彙總購買資訊與郵遞區號
+SELECT
+	c.customer_id,
+    c.customer_zip,
+    DATEDIFF(MAX(market_date), MIN(market_date))
+		AS customer_duration_days,
+	COUNT(DISTINCT market_date) AS number_of_markets,
+    ROUND(SUM(quantity * cost_to_customer_per_qty), 2)
+		AS total_spent,
+	ROUND(SUM(quantity * cost_to_customer_per_qty)
+		/ COUNT(DISTINCT market_date), 2)
+        AS average_spent_per_market
+FROM farmers_market.customer AS c
+	LEFT JOIN farmers_market.customer_purchases AS pc
+		ON c.customer_id = pc.customer_id
+GROUP BY c.customer_id;
+
+# 將顧客購買資訊與人口統計資料結合
+SELECT
+	c.customer_id,
+    DATEDIFF(MAX(market_date), MIN(market_date))
+		AS customer_duration_days,
+	COUNT(DISTINCT market_date) AS number_of_markets,
+    ROUND(SUM(quantity * cost_to_customer_per_qty), 2)
+		AS total_spent,
+	ROUND(SUM(quantity * cost_to_customer_per_qty)
+		/ COUNT(DISTINCT market_date), 2)
+        AS average_spent_per_market,
+	c.customer_zip,
+    z.median_household_income AS zip_median_household_income,
+    z.percent_high_income AS zip_percent_high_income,
+    z.percent_under_18 AS zip_percent_under_18,
+    z.percent_over_65 AS zip_percent_over_65,
+    z.people_per_sq_mile AS zip_people_per_sq_mile,
+    z.latitude,
+    z.longitude
+    
+FROM farmers_market.customer AS c
+	LEFT JOIN farmers_market.customer_purchases AS pc
+		ON c.customer_id = pc.customer_id
+	LEFT JOIN farmers_market.zip_data z
+		ON c.customer_zip = z.zip_code_5
+GROUP BY c.customer_id;
+
+# 計算顧客居住地區與農夫市場的距離
+SELECT
+	c.customer_id,
+    DATEDIFF(MAX(market_date), MIN(market_date))
+		AS customer_duration_days,
+	COUNT(DISTINCT market_date) AS number_of_markets,
+    ROUND(SUM(quantity * cost_to_customer_per_qty), 2)
+		AS total_spent,
+	ROUND(SUM(quantity * cost_to_customer_per_qty)
+		/ COUNT(DISTINCT market_date), 2)
+        AS average_spent_per_market,
+	c.customer_zip,
+    z.median_household_income AS zip_median_household_income,
+    z.percent_high_income AS zip_percent_high_income,
+    z.percent_under_18 AS zip_percent_under_18,
+    z.percent_over_65 AS zip_percent_over_65,
+    z.people_per_sq_mile AS zip_people_per_sq_mile,
+    
+    ROUND(2 * 3961 * ASIN(SQRT(POWER(SIN(RADIANS(
+		 (z.latitude - 38.4463) / 2)),2) +
+         COS(RADIANS(38.4463)) * COS(RADIANS(z.latitude)) *
+         POWER((SIN(RADIANS((z.longitude - -78.8712) / 2))), 2))))
+         AS zip_miles_from_market
+    
+FROM farmers_market.customer AS c
+	LEFT JOIN farmers_market.customer_purchases AS pc
+		ON c.customer_id = pc.customer_id
+	LEFT JOIN farmers_market.zip_data z
+		ON c.customer_zip = z.zip_code_5
+GROUP BY c.customer_id;
+
+# 提供更多的分析想法
+WITH customer_and_zip_data as
+(
+	SELECT
+		c.customer_id,
+		DATEDIFF(MAX(market_date), MIN(market_date))
+			AS customer_duration_days,
+		COUNT(DISTINCT market_date) AS number_of_markets,
+		ROUND(SUM(quantity * cost_to_customer_per_qty), 2)
+			AS total_spent,
+		ROUND(SUM(quantity * cost_to_customer_per_qty)
+			/ COUNT(DISTINCT market_date), 2)
+			AS average_spent_per_market,
+		c.customer_zip,
+		z.median_household_income AS zip_median_household_income,
+		z.percent_high_income AS zip_percent_high_income,
+		z.percent_under_18 AS zip_percent_under_18,
+		z.percent_over_65 AS zip_percent_over_65,
+		z.people_per_sq_mile AS zip_people_per_sq_mile,
+		
+		ROUND(2 * 3961 * ASIN(SQRT(POWER(SIN(RADIANS(
+			 (z.latitude - 38.4463) / 2)),2) +
+			 COS(RADIANS(38.4463)) * COS(RADIANS(z.latitude)) *
+			 POWER((SIN(RADIANS((z.longitude - -78.8712) / 2))), 2))))
+			 AS zip_miles_from_market
+		
+	FROM farmers_market.customer AS c
+		LEFT JOIN farmers_market.customer_purchases AS pc
+			ON c.customer_id = pc.customer_id
+		LEFT JOIN farmers_market.zip_data z
+			ON c.customer_zip = z.zip_code_5
+	GROUP BY c.customer_id
+)
+
+SELECT
+	cz.customer_zip,
+    COUNT(cz.customer_id) AS customer_count,
+    ROUND(AVG(cz.total_spent)) AS average_total_spent,
+    MIN(cz.zip_miles_from_market) AS zip_miles_from_market
+FROM customer_and_zip_data AS cz
+GROUP BY cz.customer_zip;
+
+# 13.5 價格分布與高低價分析資料集
+# 取產品價格的原始資料
+SELECT
+	p.product_id,
+    p.product_name,
+    p.product_category_id,
+    p.product_qty_type,
+    vi.vendor_id,
+    vi.market_date,
+    SUM(vi.quantity),
+    AVG(vi.original_price)
+FROM farmers_market.product AS p
+	LEFT JOIN farmers_market.vendor_inventory AS vi
+		ON p.product_id = vi.product_id
+GROUP BY
+	p.product_id,
+    p.product_name,
+    p.product_category_id,
+    p.product_qty_type,
+    vi.vendor_id,
+    vi.market_date;
+    
+# 納入季節因素
+SELECT
+	p.product_id,
+    p.product_name,
+    p.product_category_id,
+    p.product_qty_type,
+    vi.vendor_id,
+    MIN(MONTH(vi.market_date)) AS month_market_season_sort,
+    md.market_year,
+    md.market_season,
+    SUM(vi.quantity),
+    AVG(vi.original_price)
+FROM farmers_market.product AS p
+	LEFT JOIN farmers_market.vendor_inventory AS vi
+		ON p.product_id = vi.product_id
+	LEFT JOIN farmers_market.market_date_info AS md
+		ON vi.market_date = md.market_date
+GROUP BY
+	p.product_id,
+    p.product_name,
+    p.product_category_id,
+    p.product_qty_type,
+    vi.vendor_id,
+    md.market_year,
+    md.market_season;
+    
+# 改用窗口函數
+SELECT
+	p.product_id,
+    p.product_name,
+    p.product_category_id,
+    p.product_qty_type,
+    vi.vendor_id,
+    MIN(MONTH(vi.market_date)) OVER (PARTITION BY md.market_season) 
+		AS month_market_season_sort,
+    md.market_year,
+    md.market_season,
+    vi.original_price
+FROM farmers_market.product AS p
+	LEFT JOIN farmers_market.vendor_inventory AS vi
+		ON p.product_id = vi.product_id
+	LEFT JOIN farmers_market.market_date_info AS md
+		ON vi.market_date = md.market_date;
+        
+# 納入實際銷售量與銷售額
+SELECT
+	sub.product_id,
+    sub.product_name,
+    sub.product_category_id,
+    sub.product_qty_type,
+    sub.vendor_id,
+    sub.month_market_season_sort,
+    sub.market_year,
+    sub.market_season,
+    AVG(sub.original_price) AS avg_original_price,
+    SUM(sub.quantity) AS quantity_sold,
+    SUM(sub.quantity * sub.cost_to_customer_per_qty)
+		AS total_sales
+FROM
+(
+	SELECT
+		p.product_id,
+		p.product_name,
+		p.product_category_id,
+		p.product_qty_type,
+		vi.vendor_id,
+		MIN(MONTH(vi.market_date)) OVER (PARTITION BY md.market_season) 
+			AS month_market_season_sort,
+		md.market_year,
+		md.market_season,
+		vi.original_price,
+        cp.quantity,
+        cp.cost_to_customer_per_qty
+	FROM farmers_market.product AS p
+		LEFT JOIN farmers_market.vendor_inventory AS vi
+			ON p.product_id = vi.product_id
+		LEFT JOIN farmers_market.market_date_info AS md
+			ON vi.market_date = md.market_date
+		LEFT JOIN farmers_market.customer_purchases AS cp
+			ON vi.product_id = cp.product_id
+				AND vi.vendor_id = cp.vendor_id
+                AND vi.market_date = cp.market_date
+) AS sub
+GROUP BY
+	sub.product_id,
+    sub.product_name,
+    sub.product_category_id,
+    sub.product_qty_type,
+    sub.vendor_id,
+    sub.month_market_season_sort,
+    sub.market_year,
+    sub.market_season;
+
+# 改用市集季節的角度將價格分級    
+# 將每個季度內的價格分為低中高
+SELECT
+	sub.market_season,
+    sub.market_year,
+    sub.month_market_season_sort,
+    sub.original_price,
+	NTILE(3) OVER (PARTITION BY sub.market_year, sub.market_season
+		ORDER BY sub.original_price) AS price_ntile,
+	NTILE(3) OVER (PARTITION BY sub.market_year, sub.market_season
+		ORDER BY sub.original_price DESC) AS price_ntile_desc,
+	COUNT(DISTINCT CONCAT(sub.product_id, sub.vendor_id))
+		AS product_count,
+    SUM(sub.quantity) AS quantity_sold,
+    SUM(sub.quantity * sub.cost_to_customer_per_qty)
+		AS total_sales
+FROM
+(
+	SELECT
+		md.market_season,
+        md.market_year,
+		MIN(MONTH(vi.market_date)) OVER (PARTITION BY md.market_season) 
+			AS month_market_season_sort,
+		vi.product_id,
+		vi.vendor_id,
+		vi.original_price,
+        cp.quantity,
+        cp.cost_to_customer_per_qty
+	FROM farmers_market.product AS p
+		LEFT JOIN farmers_market.vendor_inventory AS vi
+			ON p.product_id = vi.product_id
+		LEFT JOIN farmers_market.market_date_info AS md
+			ON vi.market_date = md.market_date
+		LEFT JOIN farmers_market.customer_purchases AS cp
+			ON vi.product_id = cp.product_id
+				AND vi.vendor_id = cp.vendor_id
+                AND vi.market_date = cp.market_date
+	WHERE md.market_year IS NOT NULL
+) AS sub
+GROUP BY
+	sub.market_year,
+    sub.market_season,
+    sub.month_market_season_sort,
+    sub.original_price;
+    
+# 建立價格分析資料集
+WITH sub AS
+(
+	SELECT
+		md.market_season,
+        md.market_year,
+		MIN(MONTH(vi.market_date)) OVER (PARTITION BY md.market_season) 
+			AS month_market_season_sort,
+		vi.product_id,
+		vi.vendor_id,
+		vi.original_price,
+        cp.quantity,
+        cp.cost_to_customer_per_qty
+	FROM farmers_market.product AS p
+		LEFT JOIN farmers_market.vendor_inventory AS vi
+			ON p.product_id = vi.product_id
+		LEFT JOIN farmers_market.market_date_info AS md
+			ON vi.market_date = md.market_date
+		LEFT JOIN farmers_market.customer_purchases AS cp
+			ON vi.product_id = cp.product_id
+				AND vi.vendor_id = cp.vendor_id
+                AND vi.market_date = cp.market_date
+	WHERE md.market_year IS NOT NULL
+),
+hl AS
+(
+	SELECT
+		sub.*,
+		NTILE(3) OVER (PARTITION BY sub.market_year, sub.market_season
+			ORDER BY sub.original_price) AS price_ntile,
+		NTILE(3) OVER (PARTITION BY sub.market_year, sub.market_season
+			ORDER BY sub.original_price DESC) AS price_ntile_desc
+	FROM sub
+)
+SELECT
+	hl.market_year,
+    hl.market_season,
+    hl.price_ntile,
+    COUNT(DISTINCT CONCAT(hl.product_id, hl.vendor_id))
+		AS product_count,
+	SUM(hl.quantity) AS quantity_sold,
+    MIN(hl.original_price) AS min_price,
+    MAX(hl.original_price) AS max_price,
+    SUM(hl.quantity * hl.cost_to_customer_per_qty)
+		AS total_sales
+	FROM hl
+	GROUP BY
+		hl.market_year,
+        hl.market_season,
+        hl.price_ntile,
+        hl.month_market_season_sort
+	ORDER BY
+		hl.market_year,
+        hl.month_market_season_sort;
--- SQL for Data Scientist/14-STORE_MODIFY.sql
+++ SQL for Data Scientist/14-STORE_MODIFY.sql
@@ -30,3 +30,110 @@ DROP VIEW farmers_market.product_units_vw;
 
 # 14.2
 # 加入時間戳記欄位
+CREATE TABLE farmers_market.product_units AS
+(
+	SELECT *,
+    CURRENT_TIMESTAMP AS snapshot_timestamp
+    FROM farmers_market.product
+    WHERE product_qty_type = 'unit'
+);
+
+SELECT * FROM farmers_market.product_units;
+
+# 用INSERT INTO 插入新的列資料
+INSERT INTO farmers_market.product_units
+	(product_id, product_name, product_size, product_category_id,
+     product_qty_type, snapshot_timestamp)
+	SELECT
+		product_id,
+        product_name,
+        product_size,
+        product_category_id,
+        product_qty_type,
+        CURRENT_TIMESTAMP
+	FROM farmers_market.product
+    WHERE product_id = 23;
+
+SELECT * FROM farmers_market.product_units;
+
+# 用 DELETE 刪除符合條件的列資料
+DELETE FROM farmers_market.product_units
+WHERE product_id = 23
+	AND snapshot_timestamp = '2025-04-14 16:23:31';
+    
+SELECT * FROM farmers_market.product_units;
+
+# 用 UPDATE 更新現有的列資料
+# 更新資料前 先將資料存一份快照
+CREATE TABLE farmers_market.vendor_booth_log AS
+(
+	SELECT
+		vba.*,
+        b.booth_type,
+        v.vendor_name,
+        CURRENT_TIMESTAMP AS snapshot_timestamp
+    FROM farmers_market.vendor_booth_assignments AS vba
+		INNER JOIN farmers_market.vendor AS v
+			ON vba.vendor_id = v.vendor_id
+		INNER JOIN farmers_market.booth AS b
+			ON vba.booth_number = b.booth_number
+	WHERE market_date >= '2020-10-01'
+); 
+
+SELECT *
+FROM farmers_market.vendor_booth_log;
+
+# 更新2020-10-10 供應商8的攤位編號為11 
+UPDATE farmers_market.vendor_booth_assignments
+SET booth_number = 11
+WHERE vendor_id = 8 and market_date = '2020-10-10';
+
+# 刪除2020-10-10 供應商7 的列資料
+DELETE FROM farmers_market.vendor_booth_assignments
+WHERE vendor_id = 7 and market_date = '2020-10-10';
+
+SELECT *
+FROM farmers_market.vendor_booth_assignments
+WHERE market_date = '2020-10-10';
+
+# 將剛剛的改動插入至log中
+INSERT INTO farmers_market.vendor_booth_log
+    (vendor_id, booth_number, market_date, booth_type,
+     vendor_name, snapshot_timestamp)
+SELECT
+    vba.vendor_id,
+    vba.booth_number,
+    vba.market_date,
+    b.booth_type,
+    v.vendor_name,
+    CURRENT_TIMESTAMP AS snapshot_timestamp
+FROM farmers_market.vendor_booth_assignments AS vba
+    INNER JOIN farmers_market.vendor AS v
+      ON vba.vendor_id = v.vendor_id
+    INNER JOIN farmers_market.booth AS b
+      ON vba.booth_number = b.booth_number
+WHERE market_date >= '2020-10-01';
+
+SELECT *
+FROM farmers_market.vendor_booth_log;
+
+# practice
+# 如果是VIEW 中的CURRENT_TIMESTAMP 會是什麼
+# 會是每次執行程式的時間 因為VIEW 不會存取資料
+
+# 查詢log 表格 最新的2020-10-03的攤位資料
+SELECT x.* FROM
+(
+  SELECT
+    vendor_id,
+    booth_number,
+    market_date,
+    snapshot_timestamp,
+    MAX(snapshot_timestamp) OVER (PARTITION BY
+    vendor_id, booth_number) AS max_timestamp_in_filter
+  FROM farmers_market.vendor_booth_log
+  WHERE DATE(market_date) <= '2020-10-04'
+) AS x
+
+WHERE x.snapshot_timestamp = x.max_timestamp_in_filter
+	AND market_date = '2020-10-03';
